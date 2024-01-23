cf0a20fc6c9c81cea442af240b592f4ec62c97d2
RitikSharma125 <146950149+RitikSharma125@users.noreply.github.com>
2024-01-23 23:30:12 +0530

    test

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 const express = require('express')
 const app = express()
 
-const bodyParser = require('body-parser');
-const db = require('./database/mongoDB');
-const admin=""
-const firebase =""
-const cors = require('cors');
-
-
-app.use(express.json({ limit: '50mb' }));
-app.use(cors({ origin: '*' }));
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ limit: '10mb', type: '*/*' }));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-var AuthApi = require("./Auth/auth");
-AuthApi(app, db, admin, firebase);
-
-
-var User = require("./User/main");
-User(app, db, admin, firebase);
-
-
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
