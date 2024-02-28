97ca5660de1d93d9dff74bed6f8f2d36215e78c2
RitikSharma125 <146950149+RitikSharma125@users.noreply.github.com>
2024-02-28 15:58:27 +0530

    hamitaj

--- index.js
+++ index.js
@@ -109,3 +109,11 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 4000)
