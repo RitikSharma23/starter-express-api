638e3ee481fe5b0fdf073dbedf836b98572d0b48
RitikSharma125 <146950149+RitikSharma125@users.noreply.github.com>
2024-02-28 16:31:53 +0530

    test

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(bodyParser.raw({ limit: '10mb', type: '*/*' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.use(bodyParser.json());
+app.use(cors()); // Use the cors middleware
+
+const url = 'mongodb+srv://ljexam:LjExam@ljexam.vysc2ku.mongodb.net';
+const dbName = 'location'; // Your database name
+const collectionName = 'pincodes'; // Your collection name
+
+
 
 
 app.all('/', (req, res) => {
